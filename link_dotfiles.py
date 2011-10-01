f4c0874673c2e148274000f094a453e80c227ec9
Zahan Malkani <zahanm@gmail.com>
2011-10-01 02:35:03 -0700

    Script to symlink dotfiles
    Didn't want home directory to show up as a git repo
    Screws up repo detection in all other directories
    
    This is much cleaner

---
+++ link_dotfiles.py
@@ -0,0 +1,25 @@
+
+from __future__ import print_function
+import os
+import os.path as path
+
+# no readme, .git and script to be symlinked
+excluded = frozenset(['.git', 'README.md', __file__])
+
+def symlink_file(fname, repo, dest):
+  target = path.join(repo, fname)
+  link = path.join(dest, '.' + fname)
+  if path.lexists(link):
+    os.remove(link)
+  os.symlink(target, link)
+  print(link)
+
+def link_dotfiles():
+  home = path.expanduser('~')
+  dotfile_repo = path.dirname(path.abspath(__file__))
+  for fname in os.listdir(dotfile_repo):
+    if fname not in excluded:
+      symlink_file(fname, dotfile_repo, home)
+
+if __name__ == '__main__':
+  link_dotfiles()
