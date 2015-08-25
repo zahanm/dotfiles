39d0a4762c6a2e62db8db4e5c280d2c0afc68b40
Zahan Malkani <zahanm@fb.com>
2015-08-25 13:29:02 -0700

    finding hg root in fish
    hat-tip http://patrickoscity.de/blog/building-a-fast-mercurial-prompt/

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -16,7 +16,29 @@ function fish_prompt --description 'Write out the prompt'
   set -l cwd (set_color $color_cwd) (prompt_pwd) (set_color normal)
   set -l git_branch (__fish_git_prompt)
 
-  echo -n -s $user @ $host ' ' $cwd $git_branch $suffix ' '
+  # jumping hoops for hg
+  function find_hg_root
+    set -l dir (pwd)
+    set -e HG_ROOT
+
+    while test $dir != "/"
+      if test -f $dir'/.hg/dirstate'
+        set -g HG_ROOT $dir"/.hg"
+        return 0
+      end
+
+      set -l dir (dirname $dir)
+    end
+
+    return 1
+  end
+
+  set -l hg_branch
+  if find_hg_root 2>&1
+    set hg_branch ' ('(cat "$hg_root/branch" 2>/dev/null; or hg branch)')'
+  end
+
+  echo -n -s $user @ $host ' ' $cwd $git_branch $hg_branch $suffix ' '
 
 end
 
