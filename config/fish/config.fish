84cbf527189ae90a429f7b91f4e92f7f779a9685
Zahan Malkani <zahanm@fb.com>
2016-05-18 11:35:13 -0700

    android env vars in fish

---
+++ config/fish/config.fish
@@ -0,0 +1,13 @@
+
+set android_sdk '/opt/android_sdk'
+if test -d $android_sdk
+  set -x ANDROID_SDK $android_sdk
+  set -x ANDROID_HOME $android_sdk
+  set -x PATH $PATH "$android_sdk/tools" "$android_sdk/platform-tools"
+end
+
+set android_ndk '/opt/android_ndk'
+if test -d $android_ndk
+  set -x ANDROID_NDK_REPOSITORY $android_ndk
+end
+
