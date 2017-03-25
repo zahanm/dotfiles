4c60573a0b2ed60d1e0809ff8abe0921793d926d
Zahan Malkani <zahanm@fb.com>
2017-03-25 11:44:02 -0700

    set by chef now

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,18 +1,6 @@
 
 fish_vi_key_bindings
 
-set android_sdk '/opt/android_sdk'
-if test -d $android_sdk
-  set -x ANDROID_SDK $android_sdk
-  set -x ANDROID_HOME $android_sdk
-  set -x PATH $PATH "$android_sdk/tools" "$android_sdk/platform-tools"
-end
-
-set android_ndk '/opt/android_ndk'
-if test -d $android_ndk
-  set -x ANDROID_NDK_REPOSITORY $android_ndk
-end
-
 # add rust binaries to path
 set rust_bin "$HOME/.cargo/bin"
 if test -d $rust_bin
