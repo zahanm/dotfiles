e674292466d9cbd1d822c652312ec018669b8d57
Zahan Malkani <zahanm@fb.com>
2017-05-08 22:01:22 -0700

    chef did a lousy job with android vars

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -6,3 +6,17 @@ set rust_bin "$HOME/.cargo/bin"
 if test -d $rust_bin
   set -x PATH $PATH $rust_bin
 end
+
+# chef is not doing a great job of setting these
+# copied from ~/.fbchef/environment
+set android_sdk '/opt/android_sdk'
+if test -d $android_sdk
+  set -x ANDROID_SDK $android_sdk
+  set -x ANDROID_HOME $android_sdk
+  set -x PATH $PATH "$android_sdk/tools" "$android_sdk/platform-tools"
+end
+
+set android_ndk '/opt/android_ndk'
+if test -d $android_ndk
+  set -x ANDROID_NDK_REPOSITORY $android_ndk
+end
