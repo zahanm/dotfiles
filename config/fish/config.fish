51aa65c085da4717b76f54687ca00b1aac753259
Zahan Malkani <zahanm@gmail.com>
2017-10-11 01:02:07 -0700

    env variables always needed

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -4,3 +4,8 @@ set rust_bin "$HOME/.cargo/bin"
 if test -d $rust_bin
   set -x PATH $PATH $rust_bin
 end
+
+# make directory colors nice
+set -x LSCOLORS 'GxFxCxDxBxegedabagaced'
+
+set -x EDITOR 'vim'
