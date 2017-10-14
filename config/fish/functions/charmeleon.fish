1c23388e36ac4549a43a9f270e0239beeee9bfd2
Zahan Malkani <zahanm@fb.com>
2017-10-14 13:31:01 -0400

    standardize pokemon

---
+++ config/fish/functions/charmeleon.fish
@@ -0,0 +1,3 @@
+function charmeleon -d "Connect to the cloud"
+  command et devvm083.ftw1.facebook.com
+end
--- config/fish/functions/pika.fish
+++
@@ -1,3 +0,0 @@
-function pika -d "Connect to the cloud"
-  command et dev9126.prn1.facebook.com:8080
-end
