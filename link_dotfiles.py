36387944e5a9d61bf97c0b52612e180a7826f683
Zahan Malkani <zahanm@fb.com>
2018-11-09 11:42:21 -0800

    [symlinks] introduce a dry run mode by default

--- link_dotfiles.py
+++ link_dotfiles.py
@@ -1,9 +1,11 @@
 
 from __future__ import print_function
+import argparse
 import os
 import os.path as path
 
 EXCLUDED = frozenset(['.git', 'README.md', 'archive', __file__])
+FOR_REAL = False
 
 
 def link_dotfiles(src_dir, dest_dir):
@@ -17,21 +19,32 @@ def link_dotfiles(src_dir, dest_dir):
         if path.isdir(src):
             dest = path.join(dest_dir, fname)
             if not path.isdir(dest):
-                os.mkdir(dest)
+                if not FOR_REAL:
+                    print('DRY RUN: mkdir {0}'.format(dest))
+                else:
+                    os.mkdir(dest)
             link_dotfiles(src, dest)
 
 
 def symlink_file(fname, src, dest):
-    target = path.join(src, fname)
-    link = path.join(dest, '.' + fname)
-    if path.lexists(link):
-        print('{0}: skipped, since link exists'.format(link))
+    src_file = path.join(src, fname)
+    dest_link = path.join(dest, '.' + fname)
+    if path.lexists(dest_link):
+        print('{0}: skipped, since dest exists'.format(dest_link))
         return
-    os.symlink(target, link)
-    print(link)
+    if not FOR_REAL:
+        print('DRY RUN: ln -s {0} {1}'.format(src_file, dest_link))
+    else:
+        os.symlink(src_file, dest_link)
+        print('Linked {0}'.format(dest_link))
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Link dotfiles.')
+    parser.add_argument('--for-real', help='Makes the filesystem changes', action='store_true')
+    args = parser.parse_args()
+    if args.for_real:
+        FOR_REAL = True
     dotfile_repo = path.dirname(path.abspath(__file__))
     home = path.expanduser('~')
     link_dotfiles(dotfile_repo, home)
