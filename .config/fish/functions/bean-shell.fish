eb9ea3455c18abc9ee596eba6b217abc2eebc04a
Zahan Malkani <zahanm@gmail.com>
2019-02-03 21:04:58 -0800

    missed a spot

--- .config/fish/functions/bean-shell.fish
+++ .config/fish/functions/bean-shell.fish
@@ -1,4 +1,4 @@
-function beancount -d "Prepare to count some beans"
+function bean-shell -d "Prepare to count some beans"
   cd /Users/zahanm/source/collect-beans/
   command pwd
   pipenv shell # this must be the last command, since it spawns a new shell
