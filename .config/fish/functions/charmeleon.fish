21fc7e5f7e6b245b1c0ed16053f7d0481db24849
Zahan Malkani <zahanm@fb.com>
2019-04-03 01:16:29 -0700

    use the alias for server hotnames with et

--- .config/fish/functions/charmeleon.fish
+++ .config/fish/functions/charmeleon.fish
@@ -1,3 +1,3 @@
 function charmeleon -d "Connect to the cloud"
-  command et devvm083.ftw1.facebook.com:8080
+  command et charmander:8080
 end
--- .config/fish/functions/raichu.fish
+++ .config/fish/functions/raichu.fish
@@ -1,3 +1,3 @@
 function raichu -d "Connect to the cloud"
-  command et devvm32923.prn1.facebook.com:8080
+  command et pikachu:8080
 end
