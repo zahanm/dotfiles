16ec680a4b14d25adfb7ee7e17c96db785556b64
Zahan Malkani <zahanm@fb.com>
2020-08-04 10:34:36 -0700

    Stop the morning-routine if there are uncommitted changes

--- .config/fish/functions/morning-routine.fish
+++ .config/fish/functions/morning-routine.fish
@@ -1,5 +1,9 @@
 function morning-routine -d "Pull, update, focus, and build"
   pushd $HOME/source/fbsource
+  if test (hg status | wc -l) -gt 0
+    echo "Abort! You have un-committed changes."
+    return 1
+  end
   hg pull
   and hg update fbandroid/stable
   and arc focus-android --targets threadsapp --pinned //fbandroid/java/com/instagram/threadsapp/
