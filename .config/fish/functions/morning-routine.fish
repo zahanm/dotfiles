193d4d4da2a1169e375ce3c6dd1565fa68a5665b
Zahan Malkani <zahanm@fb.com>
2020-08-04 11:55:43 -0700

    Return to the calling folder in abort conditions

--- .config/fish/functions/morning-routine.fish
+++ .config/fish/functions/morning-routine.fish
@@ -2,13 +2,13 @@ function morning-routine -d "Pull, update, focus, and build"
   pushd $HOME/source/fbsource
   if test (hg status | wc -l) -gt 0
     echo "Abort! You have un-committed changes."
-    return 1
+    popd; return 1
   end
   if test (hg log -r ". % master" | wc -l) -gt 0; and not contains -- --rebase $argv
     echo "Abort! You have local commits and didn't use --rebase."
-    return 1
+    popd; return 1
   end
-  hg pull; or return 2
+  hg pull; or popd; return 2
   if contains -- --rebase $argv
     hg rebase --dest fbandroid/stable --base .
   else
