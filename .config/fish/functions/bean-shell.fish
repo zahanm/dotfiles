8668b9f92517fd70df529e776b1412897ddffb31
Zahan Malkani <zahanm@gmail.com>
2020-11-26 19:27:22 -0800

    Use the "accounts" repo Pipfile

--- .config/fish/functions/bean-shell.fish
+++ .config/fish/functions/bean-shell.fish
@@ -1,5 +1,5 @@
 function bean-shell -d "Prepare to count some beans"
-  pushd /Users/zahanm/source/collect-beans/
+  pushd /Users/zahanm/source/accounts/
   command pwd
   pipenv shell # this must be the last command, since it spawns a new shell
   popd
