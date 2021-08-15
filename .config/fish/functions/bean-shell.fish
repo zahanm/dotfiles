d377da2b3b6befb7f37d85674cfa04daaf3c875b
Zahan Malkani <zahanm@gmail.com>
2021-08-15 10:22:46 -0700

    Update for beancount file split

--- .config/fish/functions/bean-shell.fish
+++ .config/fish/functions/bean-shell.fish
@@ -8,16 +8,16 @@ code .
 source ../collect-beans/env.fish
 
 # Update transactions
-python ../collect-beans/collector.py CONFIG.yaml --existing personal.beancount --days 10
-../collect-beans/categorise-and-insert.sh CONFIG.yaml personal.beancount collected_beans_transactions.db
+python ../collect-beans/collector.py CONFIG.yaml --existing main.beancount --days 10
+../collect-beans/categorise-and-insert.sh CONFIG.yaml 2021.beancount collected_beans_transactions.db
 # use 'Git: Open Changes' in VS Code to see what was inserted
 
 # Update balances
 python ../collect-beans/collector.py CONFIG.yaml --balance
-../collect-beans/categorise-and-insert.sh CONFIG.yaml personal.beancount collected_beans_balance.db
+../collect-beans/categorise-and-insert.sh CONFIG.yaml 2021.beancount collected_beans_balance.db
 
 # Expenses analysis
-python ../collect-beans/analysis/spending.py personal.beancount --exclude 'Expenses:Taxes' --exclude 'Expenses:Home:Payment' --exclude 'Expenses:Home:PropertyTax' --exclude 'Expenses:Reimbursable'
+python ../collect-beans/analysis/spending.py main.beancount --exclude 'Expenses:Taxes' --exclude 'Expenses:Home:Payment' --exclude 'Expenses:Home:PropertyTax' --exclude 'Expenses:Reimbursable'
 
 # Updating credentials
 python ../collect-beans/server/index.py
--- .config/fish/functions/fava-beans.fish
+++ .config/fish/functions/fava-beans.fish
@@ -1,5 +1,5 @@
 function fava-beans -d "Load up fava and get ready to analyse beancount"
   pushd /Users/zahanm/source/accounts
-  pipenv run fava personal.beancount
+  pipenv run fava main.beancount
   popd
 end
